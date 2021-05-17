4d8132d334e2335bcc0c9e288b8189cd9f470730
Shimoji, Kensuke <k.zimoc@gmail.com>
2021-05-17 19:33:55 +0900

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
+interface Storage {
+  name: string,
+  path: string,
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  // Encapsulation has to be disabled in order for the
+  // component style to apply to the select panel.
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
   title = 'multiline-mat-select';
+  selectedStorage = new FormControl('');
+  storageList: Storage[] = [
+    {
+      name: 'C drive',
+      path: 'path/to/c-drive',
+    },
+    {
+      name: 'D drive',
+      path: 'path/to/d-drive',
+    },
+    {
+      name: 'E drive',
+      path: 'path/to/e-drive/too/long/long/long/long/long/long/long/long/path',
+    },
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
